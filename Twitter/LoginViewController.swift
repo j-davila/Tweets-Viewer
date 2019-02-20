bdd69beeb9fa4e2750bec91159666b16b01238fc
Jose <josedavila@joses-mac.local>
2019-02-20 14:19:47 -0800

    1. Added image assets 2. Added launch screen and icon 3. Added login screen and a table view that will contain the tweets

---
+++ Twitter/LoginViewController.swift
@@ -0,0 +1,39 @@
+//
+//  LoginViewController.swift
+//  Twitter
+//
+//  Created by Jose  Davila on 2/20/19.
+//  Copyright © 2019 Dan. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onLoginButton(_ sender: Any) {
+        
+        let authUrl = "https://api.twitter.com/oauth/request_token"
+        TwitterAPICaller.client?.login(url: authUrl, success: {
+            self.performSegue(withIdentifier: "loginToHome", sender: self)
+        }, failure: { (Error) in
+            print("Could not log in")
+        })
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
